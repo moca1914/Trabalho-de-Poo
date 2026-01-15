ded04ff1bc25f0755bf55b5d34d4d3941a3a2a97
Moisés de Carvalho <moisesmdecarvalho@gmail.com>
2026-01-15 16:36:48 -0300

    Modify autenticarAdministrador to handle exceptions
    Update authentication method to throw exception for incorrect password.

--- Senha.java
+++ Senha.java
@@ -1,10 +1,16 @@
 package servico;
+import excecao.SenhaIncorretaException;
 
 public class SenhaAdm {
 
     private static final String SENHA_ADMIN = "admin30";
 
-    public boolean autenticarAdministrador(String senhaDigitada) {
-        return SENHA_ADMIN.equals(senhaDigitada);
+    public boolean autenticarAdministrador(String senhaDigitada) throws SenhaIncorretaException {
+
+        if (!SENHA_ADMIN.equals(senhaDigitada)) {
+            throw new SenhaIncorretaException();
+        } else {
+            return SENHA_ADMIN.equals(senhaDigitada);
+        }
     }
 }
