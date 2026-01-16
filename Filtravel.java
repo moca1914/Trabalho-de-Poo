a222a5e97aa026d80e05019c965bc063aef781b0
Moisés de Carvalho <moisesmdecarvalho@gmail.com>
2026-01-16 14:30:18 -0300

    Update Filtravel.java

--- Filtravel.java
+++ Filtravel.java
@@ -1,10 +1,11 @@
 package servico;
 
 import modelo.Produto;
+import modelo.RestricaoAlimentar;
 import java.util.List;
 
 public interface Filtravel {
 
-    List<Produto> filtrarPorRestricao(String restricao);
+    List<Produto> filtrarPorRestricao(RestricaoAlimentar restricao);
 
 }
